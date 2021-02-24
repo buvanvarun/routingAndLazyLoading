24bdaa0032c59c71dd1ee7abe5a7a9d0c16c0ab5
R P Buvan Varun <buvanvarun.acad@gmail.com>
2021-02-24 22:04:27 +0530

    initial commit

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import {Link} from 'react-router-dom';
+import {Route} from 'react-router-dom'
+import FullPost from "../FullPost/FullPost"
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+        console.log(this.props);
+        axios.get( '/posts' )
+            .then( response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                // console.log( response );
+            } )
+            .catch(error => {
+                console.log(error);
+                // this.setState({error: true});
+            });
+    }
+
+    postSelectdHandler=(id)=>{
+        this.props.history.push({pathname: '/posts/'+id});
+    }
+
+    
+
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+               // <Link key={post.id} to={`${url}/${post.id}`}>
+                <Post 
+                    key={post.id}
+                    title={post.title} 
+                    author={post.author}
+                    clicked={()=>this.postSelectdHandler(post.id)}
+                 />
+               // </Link>
+                 );
+            });
+        }
+
+        return (
+            <div>
+            <section className="Posts">
+                {posts}
+            </section>
+                <Route path={`/posts/:id`} component={FullPost} />
+            </div>
+            
+        );
+    }
+}
+
+export default Posts;
